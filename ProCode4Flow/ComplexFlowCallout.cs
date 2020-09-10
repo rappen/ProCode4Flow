56fb499c40d2a9c14f7942071e5a20e0a8ac286e
Jonas Rapp <jonas@jonasr.app>
2020-09-10 12:27:54 +0200

    Updated plugin for blog post release

--- ProCode4Flow/ComplexFlowCallout.cs
+++ ProCode4Flow/ComplexFlowCallout.cs
@@ -1,34 +1,31 @@
 ﻿using Jonas;
-using Microsoft.Xrm.Sdk;
-using System;
-using System.Linq;
 
 namespace ProCode4Flow
 {
-    public class ComplexFlowCallout : JonasPluginBase
+    public class ProCodeEscape : JonasPluginBase
     {
         public override void Execute(JonasPluginBag bag)
         {
-            var json = bag.PluginContext.GetInputParameterForgiving<string>("JSONblob");
-            var action = bag.PluginContext.GetInputParameterForgiving<OptionSetValue>("Action");
+            var json = bag.PluginContext.GetInputParameterForgiving<string>("SomeJSON");
+            var extra = bag.PluginContext.GetInputParameterForgiving<bool>("ExtraMagic");
 
-            switch (action?.Value)
+            if (string.IsNullOrWhiteSpace(json))
             {
-                case 0:
-                    json = json.Replace("{", "<").Replace("}", ">");
-                    break;
-                case 1:
-                    json = json.ToLowerInvariant();
-                    break;
-                case 3:
-                    json = json.Substring(0, Math.Min(10, json.Length));
-                    break;
-                default:
-                    bag.PluginContext.OutputParameters.AddOrUpdateIfNotNull("ErrorCode", 1);
-                    bag.PluginContext.OutputParameters.AddOrUpdateIfNotNull("ErrorMessage", $"Invalid Action: {action.Value}");
-                    return;
+                bag.PluginContext.OutputParameters.AddOrUpdateIfNotNull("ErrorCode", 1);
+                bag.PluginContext.OutputParameters.AddOrUpdateIfNotNull("ErrorMessage", "JSON is empty");
+                return;
             }
-            bag.PluginContext.OutputParameters.AddOrUpdateIfNotNull("ProCodeResult", json);
+            var result = json.DoTheFancyMagicStuff(extra);
+            bag.PluginContext.OutputParameters.AddOrUpdateIfNotNull("ResultJSON", result);
+        }
+    }
+
+    public static class Helpers
+    {
+        public static string DoTheFancyMagicStuff(this string json, bool extra)
+        {
+            // Pro Code Magic
+            return json;
         }
     }
 }
